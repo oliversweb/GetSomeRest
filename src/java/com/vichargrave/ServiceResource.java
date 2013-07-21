d230152929a672700e7b284d369197d90e2a48cf
oliversweb <billy.oliver@gmail.com>
2013-07-21 09:28:10 +0100

    source

---
+++ src/java/com/vichargrave/ServiceResource.java
@@ -0,0 +1,63 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vichargrave;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.POST;
+
+/**
+ * REST Web Service
+ *
+ * @author Administrator
+ */
+@Path("service")
+public class ServiceResource {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of ServiceResource
+     */
+    public ServiceResource() {
+    }
+
+    /**
+     * Retrieves representation of an instance of
+     * com.vichargrave.ServiceResource
+     *
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces("text/html")
+    public String getHtml() {
+        return "<h1>Get some REST!<h1>";
+    }
+
+    /**
+     * PUT method for updating or creating an instance of ServiceResource
+     *
+     * @param content representation for the resource
+     * @return an HTTP response with content of the updated or created resource.
+     */
+    @PUT
+    @Consumes("text/html")
+    public void putHtml(String content) {
+    }
+
+    @POST
+    @Consumes("application/x-www-form-urlencoded")
+    @Produces("text/plain")
+    public String postHandler(String content) {
+        return content;
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : index
+    Created on : 21-Jul-2013, 09:06:24
+    Author     : Administrator
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
